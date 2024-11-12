218f757658c9424a8f96a7e7da0718925d07fc25
taegwon <clue9986@gmail.com>
2024-11-12 19:28:04 +0900

    크롤링 준비

---
+++ server/api/kakaoApi.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const { query } = require("express");
+const apiKey = "dcba6e5cd3ecfda60e104c381a2f0d33";
+
+async function getPlace(keyword, loc) {
+  try {
+    const res = await axios.get(
+      "https://dapi.kakao.com/v2/local/search/keyword.json",
+      {
+        headers: { Authorization: "KakaoAK ${apiKey}" },
+        params: {
+          query: keyword,
+          x: 126.9784,
+          y: 37.5663,
+          radius: 5000,
+          size: 10,
+        },
+      }
+    );
+    return res.data.documents;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+module.exports = { getPlace };
--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const getPlace = require(".api/KakaoApi");
+const { error } = require("console");
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post("api/recommand", (req, res) => {
-  const { date, time, loc } = req.body;
-
-  const courses = {};
-
-  const course_list = courses[loc] || ["no course"];
-  res.json({ date, time, loc, course: course_list });
+app.post("api/recommand", async (req, res) => {
+  //const { date, time, loc } = req.body;
+  try {
+    const courses = await getPlace("카페", loc);
+    if (courses) {
+      res.json({ date, time, loc, course: courses });
+    } else {
+      res.status(500).json({ error: "fail" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "server" });
+  }
 });
 
 app.listen(port, () => {
