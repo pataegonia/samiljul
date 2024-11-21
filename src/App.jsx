c2ccbb2fc47ab59aa7668dd1142c55d3dd43bc83
taegwon <clue9986@gmail.com>
2024-11-22 00:12:39 +0900

    될껄?6

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function NotFound() {
           cursor: "pointer",
           fontSize: "1rem",
         }}
-        onClick={() => (window.location.href = "/theme")}
+        onClick={() => (window.location.href = "/samiljul/theme")}
       >
         메인으로 돌아가기
       </button>
